351a6cf3898a67efa25dc8cceb92f2985bd47b3e
Kizzie Trinh <kizzietrinh13@yahoo.com>
2024-05-05 02:48:06 -0400

    added comments into views, htmls, etc

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,19 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    approved_comment = models.BooleanField(default=False)
 
-    def publish(self):
-        self.published_date = timezone.now()
+    def approve(self):
+        self.approved_comment = True
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.text
+    
+    def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
